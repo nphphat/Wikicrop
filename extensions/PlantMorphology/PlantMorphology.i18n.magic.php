905b84b11f2e2eacce7dea4873d9e002f4bb037c
nhtdanh <kn10196@gmail.com>
2026-05-31 20:58:36 +0700

    Add extension PlantMorpholgy and document

---
+++ extensions/PlantMorphology/PlantMorphology.i18n.magic.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Magic words for extension PlantMorphology
+ */
+
+$magicWords = [];
+
+/** English (English) */
+$magicWords['en'] = [
+    'morphology' => [ 0, 'morphology' ],
+];
+
+/** Vietnamese (Tiếng Việt) */
+$magicWords['vi'] = [
+    'morphology' => [ 0, 'morphology', 'hinhthai' ],
+];
